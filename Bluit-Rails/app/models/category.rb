ee080763a8bcbb567ffd3f0207ae3c6e199ed005
Jenn Simbartl <jsimbartl_2017@depauw.edu>
2016-07-20 09:11:46 -0400

    Add files via upload

---
+++ Bluit-Rails/app/models/category.rb
@@ -0,0 +1,9 @@
+class Category < ActiveRecord::Base
+  has_many :posts
+
+  validates :name, length: { maximum: 20 }, presence: true
+  validates :title, length: { maximum: 100 }
+  validates :description, length: { maximum: 500 }
+  validates :sidebar, length: { maximum: 5120 }
+  validates :submission_text, length: { maximum: 1024 }
+end
